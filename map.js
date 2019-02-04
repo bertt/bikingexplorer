666e7c6f5bf608a7fa2cf0956e7df5bfbae63724
bert <btemme@hotmail.com>
2019-02-04 16:56:28 +0100

    get something working

--- map.js
+++ map.js
@@ -8,34 +8,85 @@ var map = new mapboxgl.Map({
     center: [5.121303,52.088551],
     zoom: 11.75,
     bearing: -2.35,
-    pitch: 60.0
+    pitch: 60.0,
+    hash: true
 });
 
-//center: start_story.center,
-//zoom: start_story.zoom,
-//bearing: start_story.bearing,
-//pitch: start_story.pitch
-// alert('hallo');
-  
-//map.flyTo(start_extent);
+map.on('load', function() {
 
-map.on("load", function(e) {
+map.addLayer({
+    'id': 'extrusion',
+    'type': 'fill-extrusion',
+    "source": {
+      "type": "geojson",
+      "data": {
+        "type": "FeatureCollection",
+        "features": []
+      }
+    },
+    'paint': {
+        "fill-extrusion-color": {"base": 1,
+        "type": "interval",
+        "property": "0",
+        "default": "#800026",
+        "stops": [[0, "#fee8c8"],
+                  [5000, "#fdbb84"],
+                  [10000, "#fc8d59"],
+                  [15000, "#ef6548"],
+                  [20000, "#d7301f"],
+                  [25000, "#b30000"],
+                  [30000, "#7f0000"]]},
+      'fill-extrusion-height': ['get', 'traffic'],
+      'fill-extrusion-base': 0,
+      'fill-extrusion-opacity': 0.9
+    }
+  });
+
+
     map.addSource("meetpunten", {
             "type": "geojson",
             "data": "./utrecht_points.geojson"
     });
     
     map.addLayer({
-        'id': 'meetpunten',
-        'source': 'meetpunten',
+        "id": "total",
         'type': 'circle',
-        "paint": {
-            "circle-color": "#FF0000",
-            "circle-radius": 5
-        }
-    })
-});
+        'paint': {
+          'circle-radius': {
+            'base': 5,
+            'stops': [
+              [12, 5],
+              [22, 5]
+            ]
+          },
+          'circle-color': '#ff7770'
+        },
+        "source": "meetpunten"
+      });
 
+    map.on('sourcedata', function(e) {
+        if (e.sourceId !== 'meetpunten') return
+        if (e.isSourceLoaded !== true) return
+
+        var data1 = {
+            "type": "FeatureCollection",
+            "features": []
+          }
+      
+          map.querySourceFeatures('meetpunten').forEach(function(f) {
+            var object = turf.centerOfMass(f)
+            var center = object.geometry.coordinates
+            var radius = 50;
+            var options = {
+              steps: 16,
+              units: 'meters',
+              properties: object.properties
+            };
+            data1.features.push(turf.circle(center, radius, options))
+          })
+          map.getSource('extrusion').setData(data1);        
+    });
+});
 map.getCanvas().style.cursor = "default";
 
 
